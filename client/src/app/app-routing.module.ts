2f4e973170d13e80130fc54f6511fe37f061e910
Rustam Mirzaev <rustam.mirzaev@softgamings.com>
2021-01-07 01:39:51 +0300

    настройка лэйаутов

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {LoginPageComponent} from './login-page/login-page.component';
+import {AuthLayoutComponent} from './shared/layouts/auth-layout/auth-layout.component';
+import {MainLayoutComponent} from './shared/layouts/main-layout/main-layout.component';
+import {RegisterPageComponent} from './register-page/register-page.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginPageComponent}
+  {
+    path: '', component: AuthLayoutComponent, children: [
+      {path: '', redirectTo: '/login', pathMatch: 'full'},
+      {path: 'login', component: LoginPageComponent},
+      {path: 'register', component: RegisterPageComponent}
+    ]
+  },
+  {
+    path: '', component: MainLayoutComponent, children: []
+  },
 ];
 
 @NgModule({
